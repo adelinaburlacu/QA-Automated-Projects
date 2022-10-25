cdc2bbc98ea2e03ccee7fd820d5d87565b3d7165
Adelina Burlacu <adelina_burlacu17@yahoo.com>
2022-10-25 15:25:59 +0300

    Demo1 si tema 1 Selenium

---
+++ Selenium1Demo/SeleniumHomeWork1/temaSelenium1Acasa.java
@@ -0,0 +1,74 @@
+package skillbrain;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.sleep;
+
+public class temaSelenium1Acasa {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver" , "Selenium1Demo/driver/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/basic-web-page-test.html");
+//Prima pagina din tema
+        //title
+        WebElement title = driver.findElement(By.cssSelector("div[class='page-body'] h1"));
+        System.out.println(title.getText());
+        System.out.println("");
+
+        //explanation
+        WebElement explanation = driver.findElement(By.cssSelector(".explanation"));
+        System.out.println(explanation.getText());
+        System.out.println("");
+
+        //first paragraph
+        WebElement firstParagraph;
+        firstParagraph = driver.findElement(By.cssSelector("#para1"));
+        System.out.println(firstParagraph.getText());
+        System.out.println("");
+
+        //second paragraph
+        WebElement secondParagraph = driver.findElement(By.cssSelector("#para2"));
+        System.out.println(secondParagraph.getText());
+        System.out.println("");
+
+//A doua pagina din tema
+
+        driver.get("https://testpages.herokuapp.com/styled/attributes-test.html");
+        //titlul 2
+        WebElement title2 = driver.findElement(By.cssSelector("div[class='page-body'] h1"));
+        System.out.println(title2.getText());
+        System.out.println("");
+
+        //explenation 2
+        WebElement explanation2 = driver.findElement(By.cssSelector(".explanation"));
+        System.out.println(explanation2.getText());
+        System.out.println("");
+
+        //subtitle
+        WebElement subtitle = driver.findElement(By.cssSelector("body > div:nth-child(1) > h2:nth-child(4)"));
+        System.out.println(subtitle.getText());
+        System.out.println("");
+
+        //inline-explanation
+        WebElement inlineExplanation = driver.findElement(By.cssSelector("body > div:nth-child(1) > p:nth-child(5)"));
+        System.out.println(inlineExplanation.getText());
+        System.out.println("");
+
+        //domAtributte
+        WebElement paragraph3 = driver.findElement(By.cssSelector("#domattributes"));
+        System.out.println(paragraph3.getText());
+        System.out.println("");
+
+        //
+
+
+
+
+
+        sleep(5000);
+        driver.close();
+    }
+}
