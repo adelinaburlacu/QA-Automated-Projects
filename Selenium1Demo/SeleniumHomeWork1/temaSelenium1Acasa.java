d08bf86968032c0c9ecd61c8360ef2411011e833
Adelina Burlacu <adelina_burlacu17@yahoo.com>
2022-10-25 20:40:15 +0300

    actualizare

--- Selenium1Demo/SeleniumHomeWork1/temaSelenium1Acasa.java
+++ Selenium1Demo/SeleniumHomeWork1/temaSelenium1Acasa.java
@@ -98,7 +98,113 @@ public class temaSelenium1Acasa {
         System.out.println(body.getText());
         System.out.println("");
 
-        //
+        //each paragraph - a doua metoda
+        WebElement para1 = driver.findElement(By.cssSelector("#p1"));
+        System.out.println(para1.getText());
+        System.out.println("");
+
+        WebElement para2 = driver.findElement(By.cssSelector("#p2"));
+        System.out.println(para2.getText());
+        System.out.println("");
+
+        WebElement para3 = driver.findElement(By.cssSelector("#p3"));
+        System.out.println(para3.getText());
+        System.out.println("");
+
+        WebElement para4 = driver.findElement(By.cssSelector("#p4"));
+        System.out.println(para4.getText());
+        System.out.println("");
+
+        WebElement para5 = driver.findElement(By.cssSelector("#p5"));
+        System.out.println(para5.getText());
+        System.out.println("");
+
+        WebElement para6 = driver.findElement(By.cssSelector("#p6"));
+        System.out.println(para6.getText());
+        System.out.println("");
+
+        WebElement para7 = driver.findElement(By.cssSelector("#p7"));
+        System.out.println(para7.getText());
+        System.out.println("");
+
+        WebElement para8 = driver.findElement(By.cssSelector("#p8"));
+        System.out.println(para8.getText());
+        System.out.println("");
+
+        WebElement para9 = driver.findElement(By.cssSelector("#p9"));
+        System.out.println(para9.getText());
+        System.out.println("");
+
+        WebElement para10 = driver.findElement(By.cssSelector("#p10"));
+        System.out.println(para10.getText());
+        System.out.println("");
+
+        WebElement para11 = driver.findElement(By.cssSelector("#p11"));
+        System.out.println(para11.getText());
+        System.out.println("");
+
+        WebElement para12 = driver.findElement(By.cssSelector("#p12"));
+        System.out.println(para12.getText());
+        System.out.println("");
+
+        WebElement para13 = driver.findElement(By.cssSelector("#p13"));
+        System.out.println(para13.getText());
+        System.out.println("");
+
+        WebElement para14 = driver.findElement(By.cssSelector("#p14"));
+        System.out.println(para14.getText());
+        System.out.println("");
+
+        WebElement para15 = driver.findElement(By.cssSelector("#p15"));
+        System.out.println(para15.getText());
+        System.out.println("");
+
+        WebElement para16 = driver.findElement(By.cssSelector("#p16"));
+        System.out.println(para16.getText());
+        System.out.println("");
+
+        WebElement para17 = driver.findElement(By.cssSelector("#p17"));
+        System.out.println(para17.getText());
+        System.out.println("");
+
+        WebElement para18 = driver.findElement(By.cssSelector("#p18"));
+        System.out.println(para18.getText());
+        System.out.println("");
+
+        WebElement para19 = driver.findElement(By.cssSelector("#p19"));
+        System.out.println(para19.getText());
+        System.out.println("");
+
+        WebElement para20 = driver.findElement(By.cssSelector("#p20"));
+        System.out.println(para20.getText());
+        System.out.println("");
+
+        WebElement para21 = driver.findElement(By.cssSelector("#p21"));
+        System.out.println(para21.getText());
+        System.out.println("");
+
+        WebElement para22 = driver.findElement(By.cssSelector("#p22"));
+        System.out.println(para22.getText());
+        System.out.println("");
+
+        WebElement para23 = driver.findElement(By.cssSelector("#p23"));
+        System.out.println(para23.getText());
+        System.out.println("");
+
+        WebElement para24 = driver.findElement(By.cssSelector("#p24"));
+        System.out.println(para24.getText());
+        System.out.println("");
+
+        WebElement para25 = driver.findElement(By.cssSelector("#p25"));
+        System.out.println(para25.getText());
+        System.out.println("");
+
+        //the first paragraph and the other paragraphs
+        WebElement para1A = driver.findElement(By.cssSelector("#p1"));
+        System.out.println(para1A.getText());
+        System.out.println("");
+
+        WebElement otherParagraphs = driver.findElement(By.cssSelector(""));
 
 
 
